ee1526cbdfddd8c2c4934e5b1217ab4500f38452
Zi Nean Teoh <zinean00@gmail.com>
2022-08-27 14:44:00 -0500

    modify tsparticles bubbles

--- src/App.svelte
+++ src/App.svelte
@@ -23,88 +23,198 @@
     let particlesUrl = 'http://foo.bar/particles.json'
 
     let particlesConfig = {
+        // particles: {
+        //     overlap: false,
+        //     color: {
+        //         value: '#ffffff',
+        //     },
+        //     links: {
+        //         enable: false,
+        //     },
+        //     move: {
+        //         enable: false,
+        //         outModes: {
+        //             bottom: 'out',
+        //             left: 'out',
+        //             right: 'out',
+        //             top: 'out',
+        //         },
+        //     },
+        //     number: {
+        //         density: {
+        //             enable: true,
+        //         },
+        //         value: 20,
+        //     },
+        //     opacity: {
+        //         value: {
+        //             min: 0.1,
+        //             max: 0.6,
+        //         },
+        //         animation: {
+        //             enable: true,
+        //             speed: 1,
+        //             minimumValue: 0.1,
+        //         },
+        //     },
+        //     shape: {
+        //         options: {
+        //             character: {
+        //                 value: ['0', '1'],
+        //                 font: 'Verdana',
+        //                 style: '',
+        //                 weight: '400',
+        //                 fill: true,
+        //             },
+        //             char: {
+        //                 value: ['1', '0'],
+        //                 font: 'Verdana',
+        //                 style: '',
+        //                 weight: '400',
+        //                 fill: true,
+        //             },
+        //         },
+        //         type: 'char',
+        //     },
+        //     size: {
+        //         value: 16,
+        //         animation: {
+        //             speed: 10,
+        //             minimumValue: 10,
+        //         },
+        //     },
+        //     stroke: {
+        //         width: 1,
+        //         color: {
+        //             value: '#ffffff',
+        //             animation: {
+        //                 '1': {
+        //                     count: 0,
+        //                     enable: false,
+        //                     offset: 0,
+        //                     speed: 1,
+        //                     decay: 0,
+        //                     sync: true,
+        //                 },
+        //                 '0': {
+        //                     count: 0,
+        //                     enable: false,
+        //                     offset: 0,
+        //                     speed: 1,
+        //                     decay: 0,
+        //                     sync: true,
+        //                 },
+        //             },
+        //         },
+        //     },
+        // },
+        fullScreen: false,
+        zIndex: 100,
         particles: {
-            overlap: false,
+            number: {
+                value: 20,
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
+            },
             color: {
                 value: '#ffffff',
             },
-            links: {
-                enable: false,
-            },
-            move: {
-                enable: false,
-                outModes: {
-                    bottom: 'out',
-                    left: 'out',
-                    right: 'out',
-                    top: 'out',
+            shape: {
+                type: 'circle',
+                stroke: {
+                    width: 0,
+                    color: '#000000',
                 },
-            },
-            number: {
-                density: {
-                    enable: true,
+                polygon: {
+                    nb_sides: 5,
                 },
-                value: 20,
+                image: {
+                    src: 'img/github.svg',
+                    width: 100,
+                    height: 100,
+                },
             },
             opacity: {
-                value: {
-                    min: 0.1,
-                    max: 0.5,
-                },
-                animation: {
-                    enable: true,
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
                     speed: 1,
-                    minimumValue: 0.1,
+                    opacity_min: 0.1,
+                    sync: false,
                 },
-            },
-            shape: {
-                options: {
-                    character: {
-                        value: ['0', '1'],
-                        font: 'Verdana',
-                        style: '',
-                        weight: '400',
-                        fill: true,
-                    },
-                    char: {
-                        value: ['1', '0'],
-                        font: 'Verdana',
-                        style: '',
-                        weight: '400',
-                        fill: true,
-                    },
-                },
-                type: 'char',
             },
             size: {
-                value: 16,
-                animation: {
-                    speed: 10,
-                    minimumValue: 10,
+                value: 10,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 80,
+                    size_min: 0.1,
+                    sync: false,
                 },
             },
-            stroke: {
-                width: 1,
-                color: {
-                    value: '#ffffff',
-                    animation: {
-                        '1': {
-                            count: 0,
-                            enable: false,
-                            offset: 0,
-                            speed: 1,
-                            decay: 0,
-                            sync: true,
-                        },
-                        '0': {
-                            count: 0,
-                            enable: false,
-                            offset: 0,
-                            speed: 1,
-                            decay: 0,
-                            sync: true,
-                        },
+            line_linked: {
+                enable: false,
+                distance: 300,
+                color: '#ffffff',
+                opacity: 0.4,
+                width: 2,
+            },
+            move: {
+                enable: true,
+                speed: 2,
+                direction: 'top',
+                random: false,
+                straight: true,
+                out_mode: 'out',
+                bounce: false,
+                vibrate: true,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200,
+                },
+            },
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: {
+                onhover: {
+                    enable: false,
+                    mode: 'repulse',
+                },
+                onclick: {
+                    enable: false,
+                    mode: 'push',
+                },
+                resize: true,
+            },
+            modes: {
+                grab: {
+                    distance: 800,
+                    line_linked: {
+                        opacity: 1,
                     },
+                },
+                bubble: {
+                    distance: 800,
+                    size: 40,
+                    duration: 2,
+                    opacity: 0.8,
+                    speed: 1,
+                },
+                repulse: {
+                    distance: 400,
+                    duration: 0.4,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+                remove: {
+                    particles_nb: 2,
                 },
             },
         },
@@ -139,13 +249,13 @@
     <Sponsors {y} />
 </div>
 
-<Particles
-    class="particles"
-    id="tsparticles"
-    options={particlesConfig}
-    on:particlesLoaded={onParticlesLoaded}
-    {particlesInit}
-/>
+<div class="absolute top-0 w-full z-10 {y < 1.5 * innerHeight ? 'hidden' : ''}">
+    <Particles
+        options={particlesConfig}
+        on:particlesLoaded={onParticlesLoaded}
+        {particlesInit}
+    />
+</div>
 
 <style lang="postcss" global>
     @tailwind base;
@@ -158,7 +268,7 @@
         user-select: none;
     }
 
-    .particles {
-        z-index: 10;
+    #tsparticles {
+        height: 700vh;
     }
 </style>
