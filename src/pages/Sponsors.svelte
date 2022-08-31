dcd21d7fd599653e1e4cbd035fcc592ebc940fdb
Zi Nean Teoh <zinean00@gmail.com>
2022-08-30 19:24:32 -0500

    temporarily removed treasure

--- src/pages/Sponsors.svelte
+++ src/pages/Sponsors.svelte
@@ -4,14 +4,18 @@
 </script>
 
 <div class="flex relative h-screen w-full text-white">
-    <div class="absolute bottom-[80px] left-[80px] z-20">
+    <!-- <div class="absolute bottom-[80px] left-[80px]">
         <TreasureChest />
-    </div>
+    </div> -->
 
     <div
         class="absolute mt-[50px] top left-1/2 transform -translate-x-1/2 z-10"
     >
         <div class="text-white text-2xl md:text-4xl lg:text-6xl">Sponsors</div>
+
+        <div class="w-full h-full">
+            <!-- Sponsors -->
+        </div>
     </div>
 
     <div class="absolute bottom-[20px] left-[20px] z-20 font-['Futura']">
