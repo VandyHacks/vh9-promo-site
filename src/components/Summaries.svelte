51604e6fa8d2ad383952a0d54551f218b3aacf1b
longjichen1 <chenjerry222@gmail.com>
2022-10-19 18:11:54 -0500

    added first two speakers

--- src/components/Summaries.svelte
+++ src/components/Summaries.svelte
@@ -2,8 +2,37 @@
     import { navigatePotrait } from './SpeakerPortrait.svelte'
 
     let summaries = [
-        'Exciting speaker series coming to you soon! Stay tuned.',
-        "We won't actually have Nemo as our speaker, but we have speakers as exciting as Nemo!",
+        `Deanna Meador is the Deputy Director at the Wond’ry, Vanderbilt University’s Innovation Center
+and the CEO of fashion technology company Couture Technologies. 
+She began Couture Technologies, alongside her cofounder Dr. Marcelino Rodriguez-Cancio, to 
+empower direct-to-consumer fashion and workwear apparel brands with virtual try on 
+technology, 3D garment creation services, and enhanced data analytics designed to reduce 
+returns and increase conversion rates. Couture Technologies is a National Science Foundation 
+funded company that was recognized as one of the Top 9 tech startups in the world during the 
+2021 Olympics of Tech and the 2022 Most Innovative Apparel Sales Software Company.
+Deanna is a successful tech entrepreneur and inventor that describes herself as never meeting 
+a challenge she didn’t like. Her love of problem solving and the ability to develop solutions that 
+impact the lives of people has driven each of her business ventures. She started her first 
+company during graduate school and grew it to multiple physical locations in 2 states until it was
+acquired by a larger company. After selling this company, she spent 7 years conducting 
+rigorous education and juvenile justice research for Vanderbilt’s Peabody Research Institute 
+(PRI). During her time at PRI, she was the resident innovator and developed a paperless data 
+collection system for collecting assessment and observational data in the field. This system has 
+been featured in multiple articles and colloquia, including the Institute of Education Science’s 
+showcase entitled, “Using Technology in Research”, and was adopted by universities across the
+U.S., South Africa, Portugal, Australia and Sweden. In 2017, she joined 3 other inventors to 
+develop Chalk Coaching, a tool for improving classroom experiences for preschool children 
+through observation and data-driven coaching. The development of Chalk was funded by the 
+National Science Foundation. 
+Deanna is the recipient of the Unsung Hero award for her work with underserved youth, as well 
+as the Young Alumni Achievement Award from her undergraduate university for her innovations 
+(the highest honor the university bestows on alumni under the age of 40, out of over 400 
+nominees). 
+Over the last 5 years, she has taught over 15 cohorts of aspiring entrepreneurs and mentored 
+over 400 teams as they evaluated new technologies and launched new ventures. Deanna really 
+enjoys working with creators, inventors, and entrepreneurs that dedicate their lives and talents 
+to making a positive impact in the world`,
+        `Jonathan Sprinkle is a Professor of Computer Science at Vanderbilt University since 2021. Prior to joining Vanderbilt he was the Litton Industries John M. Leonis Distinguished Associate Professor of Electrical and Computer Engineering at the University of Arizona, and the Interim Director of the Transportation Research Institute. In 2020 he was named a Distinguished Scholar of the University of Arizona. From 2017-2019 he served as a Program Director in Cyber-Physical Systems and Smart & Connected Communities at the National Science Foundation in the CISE Directorate. In 2013 he received the NSF CAREER award, and in 2009, he received the UA's Ed and Joan Biggers Faculty Support Grant for work in autonomous systems. His work has an emphasis for industry impact, and he was recognized with the UA "Catapult Award" by Tech Launch Arizona in 2014, and in 2012 his team won the NSF I-Corps Best Team award. His research interests and experience are in model-based approaches to cyber-physical systems, and he teaches courses ranging from systems modeling and control to mobile application development and software engineering.`,
         // 'TBD',
         // 'TBD',
     ]
